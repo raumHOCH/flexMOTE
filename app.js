7c19bffec73c8927c3d4ac9a044e78897538ee1e
Christian David <c.david@graphscape.de>
2015-08-10 18:21:27 +0200

    sample app and 10x10 simulated smartphones

--- app.js
+++ app.js
@@ -32,8 +32,16 @@ app.get('/remote-10x10.html', function(req, res) {
 // ---- socket.io --------------------------------------------------------------
 io.on('connection', function(socket) {
     console.log('a user connected');
+    //socket.broadcast.emit('chat message', "HI!");
+
     socket.on('disconnect', function() {
+        //socket.broadcast.emit('chat message', "BYE!");
         console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('chat message', msg);
     });
 });
 
---
+++ client/public/js/remote.js
@@ -0,0 +1,16 @@
+var socket = io();
+socket.on('connect', function(id) {
+    $('#whoami').html(socket.id);
+    $('body').addClass('online');
+    $('body').removeClass('offline');
+});
+
+socket.on('disconnect', function() {
+    $('#whoami').html('<em>not connected</em>');
+    $('body').addClass('offline');
+    $('body').removeClass('online');
+});
+
+socket.on('chat message', function(msg) {
+    $('#message').html(msg);
+});
