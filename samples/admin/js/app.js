f0ddc590e8529d6bd673877a3ed9aac24c492199
Christian David <c.david@graphscape.de>
2016-04-12 12:17:02 +0200

    samples, admin panel + links to related github repositories.

---
+++ samples/admin/js/app.js
@@ -0,0 +1,237 @@
+// ----- flexMOTE setup --------------------------------------------------------
+/**
+ * @public socket.io connection
+ */
+flexMOTE.connection = io('http://remote.cloudfolio.com');
+
+/**
+ * onConnect event handler
+ */
+flexMOTE.connection.on('connect', function() {
+
+    // register a channel, using a reserved room id
+    flexMOTE.register({
+        app: 'admin',
+        room: '00000',
+        secret: 'myadminsecret',
+        version: '0.1.0',
+        maxUsers: -1,
+        timeout: -1,
+        stickySessions: false
+    }, function(status, room) {
+        clearInterval(app.updateInterval);
+        app.updateInterval = setInterval(app.updateStatistics, app.UPDATE_INTERVAL);
+        app.updateStatistics();
+    });
+});
+
+/**
+ * onDisconnect event handler
+ */
+flexMOTE.connection.on('disconnect', function() {
+    clearInterval(app.updateInterval);
+});
+
+// ----- application -----------------------------------------------------------
+/**
+ * @singleton
+ */
+var app = {};
+
+/**
+ *
+ */
+app.MAX_DATA_POINTS = 12;
+
+/**
+ *
+ */
+app.UPDATE_INTERVAL = 5 * 1000;
+
+/**
+ * interval id to stop running updateStatistics interval
+ */
+app.updateInterval = -1;
+
+/**
+ * this field is updated periodically
+ */
+app.statistics = [{
+    memmory: {},
+    latency: 0,
+    appCount: 0,
+    userCount: 0,
+    rooms: []
+}];
+
+/**
+ * load data from server
+ */
+app.updateStatistics = function() {
+    console.log('updateStatistics');
+    var start = (new Date()).getTime();
+    flexMOTE.connection.emit('statistics', function(status, data) {
+        console.log(' >', status, data);
+        data.latency = (new Date()).getTime() - start;
+        app.statistics.unshift(data);
+        if (app.statistics.length > app.MAX_DATA_POINTS) {
+            app.statistics.pop();
+        }
+        app.updateGui();
+    });
+};
+
+/**
+ * update gui w/ latest statistics
+ */
+app.updateGui = function() {
+
+    // top navi
+    $('#server-name').text(flexMOTE.connection.io.uri);
+    $('#total-apps').text(app.statistics[0].appCount || 0);
+    $('#total-users').text(app.statistics[0].userCount || 0);
+    $('#latency').text((app.statistics[0].latency || 0) + " ms");
+
+    // left sidebar (applications)
+    $('#rooms').empty();
+    for (var i = 0; i < app.statistics[0].rooms.length; i++) {
+        var room = app.statistics[0].rooms[i];
+        var html = '<div class="item"><span>#' + room.id + ' | ' + room.app + '</span>';
+        html += '<br /><em>' + room.host + '</em></div>';
+        $('#rooms').append(html);
+    }
+
+    // chart options
+    var options = {
+        responsive: true,
+        animation: false,
+        showTooltips: false,
+        scaleBeginAtZero: true
+    };
+
+    // chart data
+    var labels = app.statistics.map(function(elem, idx) {
+        return (idx * app.UPDATE_INTERVAL / 1000) + "s";
+    }).reverse();
+
+    var userCounts = app.statistics.map(function(elem) {
+        return elem.userCount || 0;
+    }).reverse();
+
+    var appCounts = app.statistics.map(function(elem) {
+        return elem.appCount || 0;
+    }).reverse();
+
+    var latencies = app.statistics.map(function(elem) {
+        return elem.latency || 0;
+    }).reverse();
+
+    var cpuLoads = app.statistics.map(function(elem) {
+        return elem.cpuLoad || 0;
+    }).reverse();
+
+    // chart 1 - app count
+    var ctx = document.getElementById("chart-1").getContext("2d");
+    var data = {
+        labels: labels,
+        datasets: [{
+            label: "Apps",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: appCounts
+        }]
+    };
+    var myLineChart = new Chart(ctx).Line(data, options);
+
+    // chart 2 - user count
+    ctx = document.getElementById("chart-2").getContext("2d");
+    data = {
+        labels: labels,
+        datasets: [{
+            label: "Users",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: userCounts
+        }]
+    };
+    myLineChart = new Chart(ctx).Line(data, options);
+
+    // chart 3 - latencies
+    ctx = document.getElementById("chart-3").getContext("2d");
+    data = {
+        labels: labels,
+        datasets: [{
+            label: "Latencies",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: latencies
+        }]
+    };
+    myLineChart = new Chart(ctx).Line(data, $.extend({
+        scaleLabel: function(payload) {
+            return payload.value + "ms";
+        }
+    }, options));
+
+    // chart 4 - cpu load
+    ctx = document.getElementById("chart-4").getContext("2d");
+    data = {
+        labels: labels,
+        datasets: [{
+            label: "Latencies",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: cpuLoads
+        }]
+    };
+    myLineChart = new Chart(ctx).Line(data, $.extend({
+        scaleBeginAtZero: true,
+        scaleOverride: true,
+        scaleStepWidth: 20,
+        scaleSteps: 5,
+        scaleStartValue: 0,
+        scaleLabel: function(payload) {
+            return payload.value + "%";
+        }
+    }, options));
+};
+
+// ----- setup semantic ui behaviour -------------------------------------------
+// tooltips for top nav
+$('.top.menu i.icon').parent().popup();
+
+/**
+ * toggle application sidebar on the left
+ * @param {Object} event
+ */
+$('.ui.button.applications').on('click', function(event) {
+    $('.ui.sidebar.left').sidebar({
+        dimPage: false
+    }).sidebar('toggle');
+});
+
+/**
+ * toggle application settings on the right
+ * @param {Object} event
+ */
+$('.ui.button.settings').on('click', function(event) {
+    $('.ui.sidebar.right').sidebar({
+        dimPage: false
+    }).sidebar('toggle');
+});
---
+++ samples/hello-world/app.js
@@ -0,0 +1,102 @@
+// debug output
+$('body').append('<p>Attempting to connect to flexMOTE server....</p>');
+
+/**
+ * sample layout, containing a "Hello World!"
+ */
+var layout = {
+    action: 'set',
+    type: 'layout',
+    id: 'layout-1',
+    data: {
+        name: 'Layout 1',
+        cols: 3,
+        rows: 3,
+        elements: [
+        // first row
+        {
+            type: "Text",
+            content: "Hello World!",
+            cols: 3
+        },
+        // second row
+        {
+            cols: 3
+        },
+        // third row
+        {
+
+            cols: 3
+        }]
+    }
+};
+
+/**
+ * @public socket.io connection
+ */
+flexMOTE.connection = io('http://remote.cloudfolio.com');
+
+/**
+ * onConnect event handler
+ */
+flexMOTE.connection.on('connect', function() {
+    $('body').append('<p>Connection established, register a channel....</p>');
+
+    // register a channel, using a reserved room id
+    flexMOTE.register({
+        app: 'hello-world',
+        room: '12345',
+        secret: 'mysecret',
+        version: '0.1.0',
+        maxUsers: -1,
+        timeout: -1,
+        stickySessions: false
+    }, function(status, room) {
+
+        // generate qrcode
+        $('#qrcode').empty();
+        var qrcode = new QRCode("qrcode", {
+            text: "http://remote.cloudfolio.com/#" + room.toString(),
+            width: 256,
+            height: 256,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+
+        // some info texts...
+        var info = '<p><a target="_blank" href="http://remote.cloudfolio.com/#' + room + '">';
+        info += 'remote.cloudfolio.com<br/>#<strong>' + room + '</strong></p>';
+        $('body').append('<p>Joined room: ' + room + '</p>');
+        $('#qrcode').append(info);
+    });
+});
+
+/**
+ * onDisconnect event handler
+ */
+flexMOTE.connection.on('disconnect', function() {
+    $('body').append('<p>Disconnected from flexMOTE server. Trying to reconnect...</p>');
+    $('#qrcode').empty();
+});
+
+/**
+ * onCommand event handler
+ * @param {Object} cmd
+ */
+flexMOTE.connection.on('cmd', function(cmd) {
+    $('body').append('<p>Command received:' + JSON.stringify(cmd));
+
+    switch (cmd.action) {
+        case 'set':
+            switch (cmd.type) {
+                case 'user':
+                    if (cmd.data && cmd.data.connected) {
+                        $('body').append('<p>User connected, send "Hello World!"....</p>');
+                        flexMOTE.sendCommand(cmd.id, layout);
+                    }
+                    break;
+            }
+            break;
+    }
+});
